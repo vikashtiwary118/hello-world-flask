df98e848a25ff503e1148d214e9532f3538f5393
vikash tiwary <tiwaryvikash118@gmail.com>
2022-09-24 01:55:33 -0700

    updated file add

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
+my_awesome_app = Flask(__name__)
+
+
+@my_awesome_app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    my_awesome_app.run()
